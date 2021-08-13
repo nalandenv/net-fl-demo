a1ba3d5c92e48968277def50116cd7fd79768a63
Nikhil Nalande <nalandenv@gmail.com>
2021-08-13 19:23:23 +0530

    add collapsable recaptcha text

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,6 @@
-console.log("connected");
+const learn = document.querySelector("#learn");
+learn.addEventListener("click", () => {
+  const captcha = document.querySelector("#recaptcha-text");
+  captcha.classList.remove("hidden");
+  learn.classList.add("hidden");
+});
